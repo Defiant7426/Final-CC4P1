667be438992b22838a77c8388f12f52470717cab
Defiant7426 <pldelacruzv@gmail.com>
2024-12-08 09:01:58 -0500

    Comentarios crud

--- AppAlmacen-Java/src/main/java/almacen/HttpServerAlmacen.java
+++ AppAlmacen-Java/src/main/java/almacen/HttpServerAlmacen.java
@@ -15,17 +15,17 @@ public class HttpServerAlmacen {
         server = HttpServer.create(new InetSocketAddress(port), 0);
 
         // Handlers CRUD
-        server.createContext("/create", new CreateHandler(service, raftNode));
-        server.createContext("/read", new ReadHandler(service));
-        server.createContext("/update", new UpdateHandler(service, raftNode));
-        server.createContext("/delete", new DeleteHandler(service, raftNode));
+        server.createContext("/create", new CreateHandler(service, raftNode)); // Para crear un nuevo registro
+        server.createContext("/read", new ReadHandler(service)); // Para leer un registro
+        server.createContext("/update", new UpdateHandler(service, raftNode)); // Para actualizar un registro
+        server.createContext("/delete", new DeleteHandler(service, raftNode)); // Para eliminar un registro
 
         // RAFT Handlers
-        server.createContext("/appendEntries", new AppendEntriesHandler(raftNode));
-        server.createContext("/requestVote", new RequestVoteHandler(raftNode));
+        server.createContext("/appendEntries", new AppendEntriesHandler(raftNode)); // Para replicar logs
+        server.createContext("/requestVote", new RequestVoteHandler(raftNode)); // Para elecciones de líder
 
         // Estado del nodo
-        server.createContext("/status", new StatusHandler(raftNode));
+        server.createContext("/status", new StatusHandler(raftNode)); // Para obtener el estado del nodo
     }
 
     public void start() {
